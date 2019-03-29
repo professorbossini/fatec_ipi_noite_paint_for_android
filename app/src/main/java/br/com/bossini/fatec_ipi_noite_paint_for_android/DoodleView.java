fa3cd6e9ce0b0c89944d3d9c10736b36b7baaaec
Fábrica de Software USJT <fabricadesoftwareusjt@gmail.com>
2019-03-29 20:42:36 -0300

    Commit pré intervalo. Chegamos até o passo 10 do material.

---
+++ app/src/main/java/br/com/bossini/fatec_ipi_noite_paint_for_android/DoodleView.java
@@ -0,0 +1,11 @@
+package br.com.bossini.fatec_ipi_noite_paint_for_android;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class DoodleView extends View {
+    public DoodleView (Context context, AttributeSet set){
+        super (context, set);
+    }
+}
---
+++ app/src/main/java/br/com/bossini/fatec_ipi_noite_paint_for_android/MainActivityFragment.java
@@ -0,0 +1,6 @@
+package br.com.bossini.fatec_ipi_noite_paint_for_android;
+
+import android.support.v4.app.Fragment;
+
+public class MainActivityFragment extends Fragment {
+}
