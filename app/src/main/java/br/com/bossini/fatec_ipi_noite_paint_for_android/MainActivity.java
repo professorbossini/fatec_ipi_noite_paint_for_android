aafef99502009e1159bcd6dc1cd0797d77899cc1
Fábrica de Software USJT <fabricadesoftwareusjt@gmail.com>
2019-03-30 01:41:09 -0300

    Commit pós aula. Continuar habilitando o acelerômetro.

--- app/src/main/java/br/com/bossini/fatec_ipi_noite_paint_for_android/MainActivity.java
+++ app/src/main/java/br/com/bossini/fatec_ipi_noite_paint_for_android/MainActivity.java
@@ -1,7 +1,10 @@
 package br.com.bossini.fatec_ipi_noite_paint_for_android;
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar =
+                findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        Configuration c = getResources().getConfiguration();
+        int screenLayout = c.screenLayout;
+        int tamanhoDaTela = screenLayout
+                & Configuration.SCREENLAYOUT_SIZE_MASK;
+        if (tamanhoDaTela
+                == Configuration.SCREENLAYOUT_SIZE_XLARGE){
+            setRequestedOrientation(ActivityInfo.
+                    SCREEN_ORIENTATION_LANDSCAPE);
+        }
+        else{
+            setRequestedOrientation(ActivityInfo.
+                    SCREEN_ORIENTATION_PORTRAIT);
+        }
     }
 }
--- app/src/main/java/br/com/bossini/fatec_ipi_noite_paint_for_android/MainActivityFragment.java
+++ app/src/main/java/br/com/bossini/fatec_ipi_noite_paint_for_android/MainActivityFragment.java
@@ -1,6 +1,74 @@
 package br.com.bossini.fatec_ipi_noite_paint_for_android;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivityFragment extends Fragment {
+
+    private DoodleView doodleView;
+
+    private float acceleration;
+    private float lastAcceleration;
+    private float currentAcceleration;
+
+    private boolean dialogOnScreen = false;
+
+    private static final int ACCELERATION_THRESHOLD = 1000000;
+
+    private static final int SAVE_IMAGE_PERMISSION_REQUEST_CODE = 1;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View raiz =
+                inflater.inflate (
+                        R.layout.fragment_main,
+                        container,
+                        false
+                );
+        setHasOptionsMenu(true);
+        doodleView = raiz.findViewById(R.id.doodleView);
+        currentAcceleration = SensorManager.GRAVITY_EARTH;
+        lastAcceleration = SensorManager.GRAVITY_EARTH;
+        acceleration = 0.0f;
+        return raiz;
+    }
+
+    private SensorEventListener listener =
+            new SensorEventListener() {
+                @Override
+                public void onSensorChanged(SensorEvent event) {
+                    float x = event.values[0];
+                    float y = event.values[1];
+                    float z = event.values[2];
+
+                    lastAcceleration = currentAcceleration;
+                    currentAcceleration =
+                            x * x + y * y + z * z;
+
+                    acceleration = currentAcceleration *
+                            (currentAcceleration - lastAcceleration);
+
+                    if (acceleration > ACCELERATION_THRESHOLD){
+                        //confirmErase();
+                    }
+                }
+
+                @Override
+                public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+                }
+            };
 }
